5db6ee69c2a23d8bdd27e226985a7eab74a62c9f
Rajvansh <106300079+steve-369@users.noreply.github.com>
2023-10-01 20:00:13 +0530

    ANALYTICS

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,6 +2,16 @@
 <html lang="en">
 
 <head>
+
+    <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-2PE427JXD0"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-2PE427JXD0');
+</script>
     <!-- Google tag (gtag.js) -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-Q5DLKL5GHJ"></script>
 <script>
